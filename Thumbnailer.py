5e29c5502203d7094d7b07d7526b575c3ac51e79
Wils0248n <wils0248n@gmail.com>
2020-01-22 10:11:39 -0500

    Thumbnailer will now create thumbnail directory if it does not exist.

--- Thumbnailer.py
+++ Thumbnailer.py
@@ -6,13 +6,14 @@ from Media import Media
 
 class Thumbnailer:
 
-    def __init__(self, thumbnail_directory, height=175, width=225):
-        self.thumbnail_directory = thumbnail_directory
-        self.height = height
-        self.width = width
+    def __init__(self, thumbnail_directory, height: int = 175, width: int = 225):
+        self.__thumbnail_directory = thumbnail_directory
+        self.__create_thumbnail_directory()
+        self.__height = height
+        self.__width = width
 
     def create_thumbnail(self, media: Media) -> bool:
-        if os.path.isfile(self.thumbnail_directory + media.hash):
+        if os.path.isfile(self.__thumbnail_directory + media.hash):
             return True
 
         if media.type == Media.Type.IMAGE:
@@ -22,19 +23,26 @@ class Thumbnailer:
         if media.type == Media.Type.VIDEO:
             return self.__create_video_thumbnail(media)
 
-    def __create_image_thumbnail(self, media_image):
+    def __create_thumbnail_directory(self):
+        try:
+            os.mkdir(self.__thumbnail_directory)
+        except FileExistsError:
+            pass
+
+    def __create_image_thumbnail(self, media_image: Media) -> bool:
         try:
             image = Image.open(media_image.path)
         except UnidentifiedImageError:
             return False
-        image.thumbnail((self.width, self.height))
-        output_path = self.thumbnail_directory + media_image.hash
+        image.thumbnail((self.__width, self.__height))
+        output_path = self.__thumbnail_directory + media_image.hash
         image.save(output_path, format=image.format)
         return True
 
-    def __create_video_thumbnail(self, media_video):
-        output_path = self.thumbnail_directory + media_video.hash
-        size = str(self.height) + "x" + str(self.width)
+    def __create_video_thumbnail(self, media_video: Media) -> bool:
+        #  TODO: Test.
+        output_path = self.__thumbnail_directory + media_video.hash
+        size = str(self.__height) + "x" + str(self.__width)
         FNULL = open(os.devnull, 'w')
         subprocess.call(['ffmpeg', '-i', media_video.path, '-ss', '00:00:01.000',
             '-vframes', '1', '-s', size, '-f', 'image2', output_path], stdout=FNULL, stderr=subprocess.STDOUT)
