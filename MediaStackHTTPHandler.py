f7b19ecaa9429ee639f35f1822fb95b3e27d5b9d
Wils0248n <wils0248n@gmail.com>
2020-01-26 17:08:48 -0500

    Refactored MediaStackHTTPHandler, 404 Page is shown if media is not found, Resolves #13

--- MediaStackHTTPHandler.py
+++ MediaStackHTTPHandler.py
@@ -1,14 +1,7 @@
-import os
 from http.server import BaseHTTPRequestHandler
-from urllib.parse import unquote
-from WebGenerator import WebGenerator
-from MediaManager import MediaManager
+from QueryHandler import QueryHandler
 
-
-media_directory = "media/"
-thumbnail_directory = "thumbs/"
-media_manager = MediaManager(media_directory, thumbnail_directory)
-web_generator = WebGenerator(thumbnail_directory)
+query_handler = QueryHandler()
 
 
 class MediaStackHTTPHandler(BaseHTTPRequestHandler):
@@ -16,103 +9,25 @@ class MediaStackHTTPHandler(BaseHTTPRequestHandler):
         BaseHTTPRequestHandler.__init__(self, request, client_address, server)
 
     def do_GET(self):
-        if self.path == "/style.css":
-            self.handle_css_request()
-
-        elif str.startswith(self.path, "/media="):
-            self.handle_media_request()
-
-        elif str.startswith(self.path, "/album="):
-            self.handle_album_request()
-
-        elif str.startswith(self.path, "/" + media_directory) or str.startswith(self.path, "/" + thumbnail_directory):
-            self.handle_media_file_request()
-
-        elif str.startswith(self.path, "/?search="):
-            self.handle_search_request()
-
-        elif self.path == "/all":
-            self.handle_all_media_request()
-
-        elif str.startswith(self.path, "/all?search=") and len(self.path) > len("/all?search="):
-            self.handle_search_all_request()
-
+        response = query_handler.handle_get_request(self.path)
+        if response[1] is None:
+            self.__send_404_response()
+            self.wfile.write(bytes("404", "UTF-8"))
         else:
-            self.handle_default_request()
-
-    def handle_css_request(self):
-        self.__send_200_response('text/css')
-        self.wfile.write(read_file_bytes("/style.css"))
-
-    def handle_media_request(self):
-        self.__send_200_response('text/html')
-        media_hash = self.path.split('=')[1]
-        html_code = web_generator.generate_media_page(media_manager.find_media(media_hash))
-        self.wfile.write(bytes(html_code, 'UTF-8'))
-
-    def handle_album_request(self):
-        self.__send_200_response('text/html')
-        album_query = self.path.split('=')[1].split('/')
-        album_name = unquote(album_query[0])
-        if len(album_query) == 2:
-            current_index = int(self.path.split('/')[2])
-            html_code = web_generator.generate_album_media_page(media_manager.get_albums()[album_name],
-                                                                current_index)
-        elif len(album_query) == 1:
-            html_code = web_generator.generate_album_page(media_manager.get_albums()[album_name])
-        self.wfile.write(bytes(html_code, 'UTF-8'))
-
-    def handle_media_file_request(self):
-        self.__send_200_response('image/png')
-        self.wfile.write(read_file_bytes(unquote(self.path)))
-
-    def handle_search_request(self):
-        self.__send_200_response('text/html')
-        search_query_list = unquote(self.path.split('=')[1]).split("+")
-        search_query = " ".join(search_query_list)
-        search_query_list = [query.lower() for query in search_query_list]
-        media = media_manager.search(search_query_list)
-        html_code = web_generator.generate_search_page(media, search_query)
-        self.wfile.write(bytes(html_code, 'UTF-8'))
-
-    def handle_default_request(self):
-        self.__send_200_response('text/html')
-        html_code = web_generator.generate_index()
-        self.wfile.write(bytes(html_code, 'UTF-8'))
-
-    def handle_all_media_request(self):
-        self.__send_200_response('text/html')
-        html_code = web_generator.generate_all_page(media_manager.get_all_media(), media_manager.get_albums())
-        self.wfile.write(bytes(html_code, 'UTF-8'))
-
-    def handle_search_all_request(self):
-        self.__send_200_response('text/html')
-        search_query_list = unquote(self.path.split('=')[1]).split("+")
-        search_query = " ".join(search_query_list)
-        search_query_list = [query.lower() for query in search_query_list]
-        media = media_manager.search_all(search_query_list)
-        html_code = web_generator.generate_all_page(media, media_manager.get_albums(), search_query)
-        self.wfile.write(bytes(html_code, 'UTF-8'))
+            self.__send_200_response(response[0])
+            self.wfile.write(response[1])
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = str(self.rfile.read(content_length), "UTF-8").split("=")
-        if post_data[0] == "add_tag":
-            if str.startswith(self.path, "/media"):
-                media = media_manager.find_media(self.path.split('=')[1])
-                media.tags.append(post_data[1])
-                media_manager.update_media_tags(media)
-                self.handle_media_request()
-            elif str.startswith(self.path, "/album"):
-                album_query = self.path.split('=')[1].split('/')
-                album_name = unquote(album_query[0])
-                current_index = int(self.path.split('/')[2])
-                media = media_manager.get_albums()[album_name].media_list[current_index]
-                media.tags.append(post_data[1])
-                media_manager.update_media_tags(media)
-                self.handle_album_request()
+
+        response = query_handler.handle_post_request(self.path, post_data)
+        if response[1] is None:
+            self.__send_500_response()
+            self.wfile.write(bytes("500", "UTF-8"))
         else:
-            self.handle_default_request()
+            self.__send_200_response(response[0])
+            self.wfile.write(response[1])
 
     def __send_200_response(self, content_type):
         self.send_response(200)
@@ -124,12 +39,10 @@ class MediaStackHTTPHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-        self.wfile.write("404")
+    def __send_500_response(self):
+        self.send_response(500)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
     def log_message(self, format, *args):
         return
-
-
-def read_file_bytes(file_path: str) -> bytes:
-    with open(os.getcwd() + file_path, 'rb') as file:
-        return file.read()
---
+++ QueryHandler.py
@@ -0,0 +1,111 @@
+import os
+from typing import Tuple
+from urllib.parse import unquote
+from WebGenerator import WebGenerator
+from MediaManager import MediaManager
+
+
+class QueryHandler:
+
+    def __init__(self):
+        self.__media_directory = "media/"
+        self.__thumbnail_directory = "thumbs/"
+        self.__web_generator = WebGenerator(self.__thumbnail_directory)
+        self.__media_manager = MediaManager(self.__media_directory, self.__thumbnail_directory)
+        self.__encoding = "UTF-8"
+
+    def handle_get_request(self, request: str) -> Tuple[str, bytes]:
+        if request == "/style.css":
+            return 'text/css', read_file_bytes("/style.css")
+        if request.startswith("/media="):
+            return 'text/html', self.__handle_media_request(request)
+        if request.startswith("/album="):
+            return 'text/html', self.__handle_album_request(request)
+        if request.startswith("/" + self.__media_directory) or request.startswith("/" + self.__thumbnail_directory):
+            return '', read_file_bytes(unquote(request))
+        if request.startswith("/?search="):
+            return 'text/html', self.__handle_search_request(request)
+        if request == "/all" or request == "/all?search=":
+            return 'text/html', self.__handle_all_media_request()
+        if request.startswith("/all?search="):
+            return 'text/html', self.__handle_search_all_request(request)
+        if request == "/":
+            return 'text/html', self.__handle_index_request()
+        else:
+            return '', None
+
+    def __handle_media_request(self, request: str) -> bytes:
+        media_hash = request.split('=')[1]
+        media = self.__media_manager.find_media(media_hash)
+        if media is None:
+            return None
+        else:
+            return bytes(self.__web_generator.generate_media_page(media), self.__encoding)
+
+    def __handle_album_request(self, request: str) -> bytes:
+        album_query = request.split('=')[1].split('/')
+        album_name = unquote(album_query[0])
+        if album_name not in self.__media_manager.get_albums().keys():
+            return None
+        if len(album_query) == 2:
+            current_index = int(request.split('/')[2])
+            html_code = self.__web_generator.generate_album_media_page(
+                self.__media_manager.get_albums()[album_name], current_index)
+        elif len(album_query) == 1:
+            html_code = self.__web_generator.generate_album_page(self.__media_manager.get_albums()[album_name])
+        return bytes(html_code, 'UTF-8')
+
+    def __handle_search_request(self, request: str) -> bytes:
+        search_query_list = unquote(request.split('=')[1]).split("+")
+        search_query = " ".join(search_query_list)
+        search_query_list = [query.lower() for query in search_query_list]
+        media = self.__media_manager.search(search_query_list)
+        return bytes(self.__web_generator.generate_search_page(media, search_query), self.__encoding)
+
+    def __handle_index_request(self) -> bytes:
+        return bytes(self.__web_generator.generate_index(), self.__encoding)
+
+    def __handle_all_media_request(self) -> bytes:
+        return bytes(self.__web_generator.generate_all_page(
+            self.__media_manager.get_all_media(), self.__media_manager.get_albums()), self.__encoding)
+
+    def __handle_search_all_request(self, request: str) -> bytes:
+        search_query_list = unquote(request.split('=')[1]).split("+")
+        search_query = " ".join(search_query_list)
+        search_query_list = [query.lower() for query in search_query_list]
+        media = self.__media_manager.search_all(search_query_list)
+        return bytes(self.__web_generator.generate_all_page(
+            media, self.__media_manager.get_albums(), search_query), self.__encoding)
+
+    def handle_post_request(self, request: str, post_data: str) -> Tuple[str, bytes]:
+        if post_data[0] == "add_tag":
+            return 'text/html', self.__handle_add_tag_request(request, post_data)
+
+    def __handle_add_tag_request(self, request, post_data) -> bytes:
+        if str.startswith(request, "/media"):
+            media = self.__media_manager.find_media(request.split('=')[1])
+            media.tags.append(post_data[1])
+            self.__media_manager.update_media_tags(media)
+            return self.__handle_media_request(request)
+        elif str.startswith(request, "/album"):
+            album_query = request.split('=')[1].split('/')
+            album_name = unquote(album_query[0])
+            current_index = int(request.split('/')[2])
+            media = self.__media_manager.get_albums()[album_name].media_list[current_index]
+            media.tags.append(post_data[1])
+            self.__media_manager.update_media_tags(media)
+            return self.__handle_album_request(request)
+
+
+def read_file_bytes(file_path: str) -> bytes:
+    try:
+        with open(os.getcwd() + file_path, 'rb') as file:
+            return file.read()
+    except FileNotFoundError:
+        return None
+    except IsADirectoryError:
+        return None
+
+
+if __name__ == '__main__':
+    QueryHandler()
